054cb3582d0efc65df72998a612c69704199ca00
aliting <aliting@163.com>
2019-06-04 11:23:30 +0800

    另一个员工重写提交

--- src/main/java/com/itqf/pojo/Book.java
+++ src/main/java/com/itqf/pojo/Book.java
@@ -1,5 +1,28 @@
 package com.itqf.pojo;
 
 public class Book {
+	private  int id;//编号
+	private  String author;
+	private String bookName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	
 
 }
