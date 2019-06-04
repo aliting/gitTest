0741d6253e364c2c4c9d614d77409fe3896a2d71
aliting <aliting@163.com>
2019-06-04 10:49:06 +0800

    提交用户实体

--- src/main/java/com/itqf/pojo/Users.java
+++ src/main/java/com/itqf/pojo/Users.java
@@ -3,7 +3,14 @@ package com.itqf.pojo;
 public class Users {
 	
 	private  int id;
+	private  String  name;
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
 	public void setId(int id) {
 		this.id = id;
 	}
